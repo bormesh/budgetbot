ced72090dcd2789798cd2640789df8fb46fe0998
Rob <rob@216software.com>
2014-09-07 13:13:50 -0400

    Budget bot initial commit

---
+++ database-change-scripts/initial-setup.sql
@@ -0,0 +1,27 @@
+create table budgetbot_schema_version
+(
+    script_path citext primary key,
+    script_contents text,
+    inserted timestamp not null default now(),
+    updated timestamp
+);
+
+create or replace function set_updated_column ()
+returns trigger
+as
+$$
+
+begin
+
+    NEW.updated = now();
+    return NEW;
+
+end;
+$$
+language plpgsql;
+
+create trigger budgetbot_schema_version_set_updated_column
+before update
+on budgetbot_schema_version
+for each row
+execute procedure set_updated_column();
---
+++ setup.py
@@ -0,0 +1,21 @@
+# vim: set expandtab ts=4 sw=4 filetype=python fileencoding=utf8:
+
+from setuptools import find_packages, setup
+
+setup(
+
+    name='budgetbot',
+
+    version='0.0.1',
+
+    packages=find_packages(),
+
+    include_package_data=True,
+
+    package_dir={'budgetbot': 'budgetbot'},
+
+    scripts=[
+        'budgetbot/scripts/run-dev-webapp',
+    ],
+
+)
