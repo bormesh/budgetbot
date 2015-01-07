98b730d3ec887452f7d59d74161a888ed32df177
W. Matthew Wilson <matt@216software.com>
2015-01-07 15:27:05 -0500

    ok expense categories should work correctly now

--- database-change-scripts/add-expense-denormalized-view.sql
+++ database-change-scripts/add-expense-denormalized-view.sql
@@ -1,16 +1,5 @@
 create view expense_categories_denormalized as
 
-select (ec.*)::expense_categories as expense_category,
-                   be.budgeted_amount,
-                   be.effective, sum(e.amount) as amount_spent
-                   from expense_categories ec
-                   join budgeted_expenses as be
-                   on be.expense_category = ec.title
-                   left join expenses as e
-                   on (e.expense_category = ec.title and e.expense_date <@ be.effective)
-                   group by (ec.*),
-                   be.budgeted_amount, be.effective;
-
 SELECT ec.*::expense_categories AS expense_category,
     be.budgeted_amount,
     be.effective,
@@ -18,6 +7,6 @@ SELECT ec.*::expense_categories AS expense_category,
     array_agg((e.*)::expenses) as expenses
    FROM expense_categories ec
      JOIN budgeted_expenses be ON be.expense_category = ec.title
-     LEFT JOIN expenses e ON e.expense_category = ec.title
+     LEFT JOIN expenses e ON e.expense_category = ec.title and e.expense_date <@ be.effective
   GROUP BY ec.*, be.budgeted_amount, be.effective;
 
