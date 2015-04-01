3dc3d972295630b7a9a8adf81a14753c5c1ba927
W. Matthew Wilson <matt@216software.com>
2015-04-01 16:40:17 -0400

    added rta nextconnect bus stuff to budget bot

--- budgetbot/webapp/mockups/handlers.py
+++ budgetbot/webapp/mockups/handlers.py
@@ -2,6 +2,7 @@
 
 import logging
 import re
+import requests
 import textwrap
 
 from budgetbot.webapp.framework.handler import Handler
@@ -15,14 +16,64 @@ module_template_prefix = 'mockups'
 module_template_package = 'budgetbot.webapp.mockups.templates'
 
 
-class SplashMockup(Handler):
+class BusTimes(Handler):
 
-    route_strings = set(['GET /mockup/splash'])
+    route_strings = set(['GET /bus'])
 
     route = Handler.check_route_strings
 
     def handle(self, req):
 
-        return Response.tmpl('mockups/splash.html')
+        return Response.tmpl('mockups/bus.html')
+
+class BusTimesJSON(Handler):
+
+    route_strings = set(['GET /bus-times'])
+
+    route = Handler.check_route_strings
+
+    def handle(self, req):
+
+        # Cedar and Grandview East
+        response = requests.post(
+            'http://nextconnect.riderta.com/Arrivals.aspx/getStopTimes',
+            data = '{"routeID": "149","directionID":"3","stopID":"9081", "useArrivalTimes":"false"}',
+            headers={'Content-Type': ' application/json'})
+
+        homeeast = response.json()['d']['stops'][0]['crossings']
+
+
+        response = requests.post(
+            'http://nextconnect.riderta.com/Arrivals.aspx/getStopTimes',
+            data = '{"routeID": "103","directionID":"3","stopID":"9405", "useArrivalTimes":"false"}',
+            headers={'Content-Type': ' application/json'})
+
+        [homeeast.append(x) for x in \
+             response.json()['d']['stops'][0]['crossings']]
+
+
+        homedowntown = []
+
+        response = requests.post(
+            'http://nextconnect.riderta.com/Arrivals.aspx/getStopTimes',
+            data = '{"routeID": "103","directionID":"14","stopID":"9411", "useArrivalTimes":"false"}',
+            headers={'Content-Type': ' application/json'})
+
+        [homedowntown.append(x) for x in \
+             response.json()['d']['stops'][0]['crossings']]
+
+        response = requests.post(
+            'http://nextconnect.riderta.com/Arrivals.aspx/getStopTimes',
+            data = '{"routeID": "149","directionID":"14","stopID":"10838", "useArrivalTimes":"false"}',
+            headers={'Content-Type': ' application/json'})
+
+        [homedowntown.append(x) for x in \
+             response.json()['d']['stops'][0]['crossings']]
+
+
+
+        return Response.json({'success':'true',
+        'homedowntown':homedowntown,
+        'homeeast':homeeast})
 
 
