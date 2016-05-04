0bcb9add9cbe7811043da96eb4b4b47d573675d7
Rob Heinen <bormesh@gmail.com>
2016-05-04 12:17:54 -0400

    shopping lists now sharable between users -- need to view shared lists on shopping list page
    Former-commit-id: 92560193aac1b565822101e2c34b99f382454433

--- static/viewmodels/shoppinglistadduservm.js
+++ static/viewmodels/shoppinglistadduservm.js
@@ -5,22 +5,27 @@ function AddUserShoppingListViewModel (data) {
 
     self.rootvm = data.rootvm;
     self.shopping_list_id = ko.observable();
-    self.shopping_list = ko.observable(new ShoppingList({});
+    self.shopping_list = ko.observable(new ShoppingList({}));
 
 
     self.initialize = function(){
         self.is_busy(true);
 
         console.log('shopping list id is ', self.shopping_list_id());
-        self.shopping_list(new ShoppingList({'shopping_list_id':self.shopping_list_id()));
-        self.shopping_list.look_up_deets();
+        self.shopping_list(new ShoppingList({'shopping_list_id':self.shopping_list_id()}));
+        return (self.shopping_list().look_up_deets().then(
+            self.look_up_shopping_list_users).then(
+            function(){
+                self.is_busy(false);
 
-        self.is_busy(false);
+        }));
+
     };
 
     self.name_or_email = ko.observable();
     self.no_results_message = ko.observable(false);
     self.search_results = ko.observableArray([]);
+    self.shopping_list_users = ko.observableArray([]);
 
     self.search_button_disabled = ko.computed(function(){
         return self.is_busy() || !self.name_or_email();
@@ -30,11 +35,47 @@ function AddUserShoppingListViewModel (data) {
         return self.search_results().length > 0;
     });
 
+    self.show_users_table = ko.computed(function(){
+        return self.shopping_list_users().length > 0;
+    });
+
+    self.look_up_shopping_list_users = function(){
+
+        return $.ajax({
+            url:"/api/shopping-list-users",
+            type: "GET",
+            data:{'shopping_list_id':self.shopping_list_id()},
+            success: function (data) {
+
+                if(data.success == true){
+                   console.log('shopping list deets ', data);
+                    self.shopping_list_users(ko.utils.arrayMap(data.people,
+                    function(p){
+                        x = new Person(p);
+                        console.log(x);
+                        return x;
+                    }));
+                }
+                else if (data.success == false && data.needs_to_log_in) {
+                    toastr.error(data.message);
+                    pager.navigate("login");
+                }
+                else {
+                    toastr.error(data.message);
+                    pager.navigate("login");
+                }
+            },
+
+            failure: function(data)
+            {
+                toastr.alert("failure!")
+            }
+        });
+    };
+
     self.add_user_to_shopping_list = function(user) {
 
         self.is_busy(true);
-
-        console.log('adding user uuid: ', user.person_uuid());
 
         $.ajax({
             url:"/api/insert-shopping-list-user",
@@ -42,12 +83,18 @@ function AddUserShoppingListViewModel (data) {
             dataType:"json",
             contentType: "application/json; charset=utf-8",
             processData: false,
-            data: {'person_uuid':user.person_uuid(),
-                'shopping_list_id':self.shopping_list_id()},
-
+            data: ko.toJSON({'person_uuid':user.person_uuid(),
+                'shopping_list_id':self.shopping_list_id()}),
             success: function (data) {
                 // Recaculate new totals
                 console.log(data);
+
+                if(data.success == true){
+                    // Move user to the added people list
+                    self.shopping_list_users.push(user);
+                    // Remove from current search users
+                    self.search_results.remove(user);
+                }
 
             },
             failure: function(data){
@@ -57,6 +104,13 @@ function AddUserShoppingListViewModel (data) {
                 self.is_busy(false);
             }
         });
+    };
+
+    self.remove_user_from_shopping_list = function(user){
+
+        console.log('not implemented');
+
+
     };
 
     self.user_search = function(){
@@ -69,8 +123,8 @@ function AddUserShoppingListViewModel (data) {
             type: "GET",
             dataType:"json",
             contentType: "application/json; charset=utf-8",
-            processData: false,
-            data: {'search_query':self.name_or_email()},
+            data: {'search_query':self.name_or_email(),
+                'shopping_list_id':self.shopping_list_id()},
             success: function (data) {
                 // Recaculate new totals
                 console.log(data);
@@ -78,7 +132,6 @@ function AddUserShoppingListViewModel (data) {
                     self.search_results(ko.utils.arrayMap(data.search_results,
                     function(p){
                         x = new Person(p);
-                        console.log(x);
                         return x;
                     }));
                 }
